2a1e27e78442f1d9b1b09c84711f020ea848df77
g23988 <g23988@gmail.com>
2016-05-18 18:46:47 +0800

    fist commit

---
+++ eventLogCheck/Config.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eventLogCheck
+{
+    class Config
+    {
+
+
+        private string _iniPath = @"eventLogCheck.ini";
+        private List<int> _checkid = new List<int>();
+        private List<String> _checkword = new List<String>();
+
+        public Config() {
+            _checkid = readCheckID();
+            _checkword = readCheckword();
+        }
+
+        private List<string> readCheckword()
+        {
+            List<String> checkwork = new List<String>();
+            String keywordListString = Properties.Settings.Default["Keywords"].ToString();
+            string[] sArray = keywordListString.Split(',');
+            foreach (string item in sArray)
+            {
+                _checkword.Add(item);
+            }
+            return _checkword;
+        }
+
+        private List<int> readCheckID()
+        {
+            List<int> checkID = new List<int>();
+            String idListString = Properties.Settings.Default["IDs"].ToString();
+            string[] sArray = idListString.Split(',');
+            foreach (string item in sArray)
+            {
+                checkID.Add(Convert.ToInt32(item));
+            }
+            return checkID;
+        }
+
+        public string path {
+            get {return _iniPath;}
+            set { _iniPath = value; }
+        }
+
+        public List<int> checkid {
+            get { return _checkid; }
+            set { _checkid = value; }
+        }
+
+        public List<string> checkword 
+        {
+            get { return _checkword; }
+            set { _checkword = value; }
+        }
+
+        
+
+
+    }
+}
---
+++ eventLogCheck/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Management;
+
+
+namespace eventLogCheck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static long last = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Config config = new Config();
+
+            check();
+            
+        }
+
+        private void check()
+        {
+            listBox1.Items.Clear();
+            EventLog mylog = new EventLog();
+            mylog.Log = "security";
+            mylog.MachineName = System.Environment.MachineName;
+
+            
+
+            foreach (EventLogEntry entry in mylog.Entries)
+            {
+                if (entry.InstanceId >= last)
+                {
+                    listBox1.Items.Add(entry.TimeGenerated);
+                    listBox1.Items.Add(entry.InstanceId);
+                }
+                last = entry.InstanceId;
+            }
+           
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            check();
+        }
+    }
+}
